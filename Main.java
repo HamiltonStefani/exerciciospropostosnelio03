1fa7318783d9b092c87d26652234dc7bd727dda6
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2022-12-28 11:47:35 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package exerciciospropostosnelio03;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		System.out.println("Digite o número:");
+		num = sc.nextInt();
+		
+		if(num < 0) {
+			System.out.println("O número é negativo");
+		}else {
+			System.out.println("O número é positivo");
+		}
+		
+		sc.close();
+
+	}
+
+}
